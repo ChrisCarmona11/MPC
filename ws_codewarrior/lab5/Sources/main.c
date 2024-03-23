5d0597b75a9f5008b20dc300e11e58af25371cc8
chris carmona <chriscarmonav@gmail.com>
2024-03-23 14:21:26 +0100

    Enviado Informe Lab 5

--- ws_codewarrior/lab5/Sources/main.c
+++ ws_codewarrior/lab5/Sources/main.c
@@ -1,37 +1,24 @@
-/*
- * main.c
- *
- *  Created on: Feb 27, 2024
- *      Author: chris
- */
-/*
- * main implementation: use this 'C' sample to create your own application
- *
- */
-
 #include "derivative.h" /* include peripheral declarations */
 #include <constantes_int_1.h>
 #include <float.h>
 #include <math.h>
 
+#define LDVAL_TRIGGER_1MS 20640 // 1ms/48.45ns = 20639.83
+#define ESCALA  10.0 / 65535.0
+
 //valores para contadores
 int Flag_interrup = 0;
-static int contador_ms = 0;
-#define LDVAL_TRIGGER_1MS 20640 // 1ms/48.45ns = 20639.83
-//regist donde guardar los contadores de ms
-static int canal_int,canal;			// Variable para ir atravesando el regist
+int contador_ms   = 0;
+//registro donde guardar los contadores de ms
 
-static float scala = 10.0 / 65535.0;
-
+int canal_int;			// Variable para ir atravesando el regist
+int canal;				// Variable para ir atravesando el regist
 static int regist[128][4];
 static int buffer[128][4];
 
-extern void enable_irq(int irq); // Función para habilitar los regists de interrupción
+extern void enable_irq(int irq); // Funciï¿½n para habilitar los regists de interrupciï¿½n
 
 void pit_interrupt_config(void)
-/*
- * Contador para detectar la interrupción de milisegundo mediante un preescaler
- * */
 {
 	// Enable the clock to the PIT module
 	SIM_SCGC6 |= SIM_SCGC6_PIT_MASK;
@@ -44,31 +31,41 @@ void pit_interrupt_config(void)
 	// Clear any pending interrupt
 	PIT_TFLG0 |= PIT_TFLG_TIF_MASK;
 	// Enable interrupt
-	enable_irq(68); // regist 2 y 4 de sobra asi que el bit flag será el 4 creo.
+	enable_irq(68); // regist 2 y 4 de sobra asi que el bit flag serï¿½ el 4 creo.
 	// Enable timer and interrupt
 	PIT_TCTRL0 = (PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK);
 }
 
-float toVolts(int number) {
-	float valor = number * scala;
+float toVolts(int number) 
+{
+	float valor = number * ESCALA;
 	return valor;
 }
 
-void pit0_isr(void) {
+void pit0_isr(void)
+{
 	PIT_TFLG0 |= PIT_TFLG_TIF_MASK; //Clear LPT Compare flag
-	for (canal_int = 0; canal_int < 4; canal_int++) {
+	for (canal_int = 0; canal_int < 4; canal_int++) 
+	{
 		buffer[contador_ms][canal_int]= tabla[contador_ms][canal_int];
 	}
 	contador_ms++; //Aumentar el contador de milisegundos
-	if (contador_ms == 125){ 
-		Flag_interrup = 0;}
-	if (contador_ms == 128) {
+
+#ifdef DEBUG
+	if (contador_ms == 125)
+	{ 
+		Flag_interrup = 0;
+	}
+#endif
+	if (contador_ms == 128) 
+	{
 		Flag_interrup = 1;
-		contador_ms = 0;
+		contador_ms   = 0;
 	}
 }
 
-int main(void) {
+int main(void) 
+{
 
 	//Configuracion de los timers
 	pit_interrupt_config();
@@ -79,47 +76,56 @@ int main(void) {
 	float quadratic[4] = { 0, 0, 0, 0 };
 	float max_f[4];
 	float min_f[4];
+	float promedio[4];
+	float promedioQ[4];
 	canal = 0;
 	int muestra = 0;
-	while (1) {
-		if (Flag_interrup == 1) {
+
+	while (1) 
+	{
+		if (Flag_interrup == 1) 
+		{
 			memcpy(regist, buffer, sizeof(int) * 128 * 4); 	// volcar al registro la muesta entera
 			memset(buffer, 0, sizeof(int) * 128 * 4); 			// Limpiar el buffer
-			for (canal = 0; canal < 4; canal++) {
-				for (muestra = 0; muestra < 128; muestra++) {
-					if (regist[muestra][canal] > max[canal]) {
+	
+			for (canal = 0; canal < 4; canal++) 
+			{
+				for (muestra = 0; muestra < 128; muestra++) 
+				{
+					if (regist[muestra][canal] > max[canal]) 
+					{
 						max[canal] = regist[muestra][canal];
 					}
-					if (regist[muestra][canal] < min[canal]) {
+					if (regist[muestra][canal] < min[canal]) 
+					{
 						min[canal] = regist[muestra][canal];
 					}
+					
 					accumulative[canal] += regist[muestra][canal];
-					quadratic[canal] +=  toVolts(regist[muestra][canal])
-							* toVolts(regist[muestra][canal]);
-					if ( muestra == 127) 
+					quadratic[canal] 	+=  toVolts(regist[muestra][canal])* toVolts(regist[muestra][canal]);
+					if ( muestra == 127)
+					{
 						Flag_interrup= 0;
+					}
 				}
 			}
-			float promedio[4];
-			float promedioQ[4];
 
-			for (canal = 0; canal < 4; canal++) {
-				max_f[canal] = toVolts(max[canal]);
-				min_f[canal] = toVolts(min[canal]);
-				promedio[canal] = toVolts(
-						(int) (((float) accumulative[canal]) / 128.0));
-				promedioQ[canal] = sqrt(quadratic[canal] / 128.0);
+			for (canal = 0; canal < 4; canal++) 
+			{
+				max_f[canal] 		= toVolts(max[canal]);
+				min_f[canal] 		= toVolts(min[canal]);
+				promedio[canal] 	= toVolts((int) (((float) accumulative[canal]) / 128.0));
+				promedioQ[canal]	= sqrt(quadratic[canal] / 128.0);
 
 				//Limpiar los registros de las variables que se visualizan.
-				max[canal] = 0;
-				min[canal] = 0;
+				max[canal] 			= 0;
+				min[canal] 			= 0;
 				accumulative[canal] = 0;
-				quadratic[canal] = 0;
+				quadratic[canal] 	= 0;
 			}
 			
 		}
 	}
-
 	return 0;
 }
 
